14c1f5ac0eb7e54549dfcc5f7a2047c9d5117aa8
Ruby Mae Spurling <r.m.spurling@se25.qmul.ac.uk>
2026-05-02 18:39:39 +0100

    Added central authority service

--- src/main/java/com/settp/id/core/service/CentralAuthority.java
+++ src/main/java/com/settp/id/core/service/CentralAuthority.java
@@ -1,4 +1,47 @@
 package com.settp.id.core.service;
 
+import com.settp.id.core.exception.IdentityNotFoundException;
+import com.settp.id.core.exception.IllegalStatusChangeException;
+import com.settp.id.core.model.DigitalID;
+import com.settp.id.core.model.IdentityStatus;
+import com.settp.id.core.repository.IdentityRepository;
+
+import java.util.UUID;
+
 public class CentralAuthority {
+    private final IdentityRepository repository;
+
+    public CentralAuthority(IdentityRepository repository) {
+        this.repository = repository;
+    }
+
+    public String createIdentity() {
+        String uuid = UUID.randomUUID().toString();
+        DigitalID newIdentity = new DigitalID(uuid);
+
+        repository.save(newIdentity);
+        return uuid;
+    }
+
+    public void statusUpdate(String uuid, IdentityStatus newStatus) {
+        DigitalID identity = repository.findByUuid(uuid).orElseThrow(() -> new IdentityNotFoundException(uuid));
+
+        if (identity.getStatus() == IdentityStatus.REVOKED) {
+            throw new IllegalStatusChangeException(identity.getStatus(), "Status Update");
+        }
+
+        identity.setStatus(newStatus);
+        repository.save(identity);
+    }
+
+    public void updateAttribute(String uuid, String key, String value) {
+        DigitalID identity = repository.findByUuid(uuid).orElseThrow(() -> new IdentityNotFoundException(uuid));
+
+        if (identity.getStatus() == IdentityStatus.REVOKED) {
+            throw new IllegalStatusChangeException(identity.getStatus(), "Attribute Update");
+        }
+
+        identity.setAttribute(key, value);
+        repository.save(identity);
+    }
 }
